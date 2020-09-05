7a9eca0fd849887604309b459286571f8e778684
Nathan <nathancai1031@gmail.com>
2020-09-05 09:12:46 -0400

    gave footer its own includes and parsed the projects json file

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const port = 3000
+const fs = require('fs')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-
+const myProjects = JSON.parse(fs.readFileSync('./projects.json','utf8'));
+console.log(myProjects)
 app.get('/', (req, res, next) => {
     res.render('index')
 })
-
+app.get('/projects', (req, res, next) => {
+    res.render('projects', {
+        myProjects: myProjects
+    })
+})
 app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(process.env.PORT || port, () => {
