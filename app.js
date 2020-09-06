7c82415157ec5376f7e267d200016e4d2c6a2fab
Nathan <nathancai1031@gmail.com>
2020-09-06 16:35:10 -0400

    added angular project

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 const myProjects = JSON.parse(fs.readFileSync('./projects.json','utf8'));
-console.log(myProjects)
 app.get('/', (req, res, next) => {
     res.render('index')
 })
