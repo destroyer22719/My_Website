9c569bf2adeadd7687e57af0ad64a1ec05a4f29f
Nathan <nathancai1031@gmail.com>
2021-02-10 17:52:53 -0500

    updated the site to update skills and knowledge, basic styling and information, and deleted unnecesary code projects

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(compression())
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-const myProjects = JSON.parse(fs.readFileSync('./projects.json','utf8'));
+const myProjects = require("./projects.json");
+
 app.get('/', (req, res, next) => {
     res.render('index')
 })
